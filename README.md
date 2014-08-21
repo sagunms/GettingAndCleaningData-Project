1317bc2297f9b43b635f47c61c439d7cb3ff5f70
Sagun Man Singh Shrestha <sagunms@msn.com>
2014-08-22 01:54:11 +1000

    Included project documentation

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-GettingAndCleaningData-Project
-==============================
+Coursera Getting and Cleaning Data Course Project
+==================
 
-Project for Coursera's Getting and Cleaning Data course
+This is a peer-assessed assignment for Getting and Cleaning Data course using R programming language.
+
+`run_analysis.R` is a script that tidies up and produces aggregate data for dataset downloaded from: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+The training and test sets are merged together, provided with meaningful labels, activity labels are converted to sensible names and only the mean and standard deviation observations are kept. This script produces mean for all subject-activity groups, and writes a text file called `TidyData.txt` containing tidy data. In addition, the script also auto-generates the `CodeBook.md` file as a summary of the resulting data sets. 
+
+The `run_analysis.R` script is commented to indicate which parts of the code are responsible for which transformations. After downloading the dataset, please edit the `datasetLocation` and `workspaceLocation` variables according to your desired location.
--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,53 @@
-# Start
+## Main R script for data analysis
+
+# Initial path configuration
+datasetLocation <- "/Users/sagunms/Dropbox/Coursera/Data Science/Getting and Cleaning Data/Project/UCI HAR Dataset/"
+workspaceLocation <- "/Users/sagunms/Dropbox/Coursera/Data Science/Getting and Cleaning Data/Project/GettingAndCleaningData-Project/"
+setwd(workspaceLocation)
+
+#install.packages('plyr')
+library('plyr') # For ddply function
+
+# Read table helper function
+readTable <- function(file) {
+    read.table(paste(datasetLocation, file, ".txt", sep=""))
+} 
+
+# Load all training and test data
+trainData <- readTable("train/X_train")
+trainLabel <- readTable("train/Y_train")
+trainSubject <- readTable("train/subject_train")
+testData <- readTable("test/X_test")
+testLabel <- readTable("test/Y_test")
+testSubject <- readTable("test/subject_test")
+featureName <- readTable("features")
+activityLabel <- readTable("activity_labels")
+
+# Merge training and test data sets to create one data set
+allData <- rbind(trainData, testData)
+colnames(allData) <- featureName$V2
+
+# Extract only measurements on the mean and standard deviation for each measurement
+allData <- allData[grep("mean\\(\\)|std\\(\\)", names(allData))]
+
+# Bind activity names and subjects to respective names in the data set
+allLabels = rbind(trainLabel, testLabel)
+allSubjects = rbind(trainSubject, testSubject)
+allLabels = merge(allLabels, activityLabel, by.x="V1", by.y="V1", all=TRUE)
+colnames(allLabels) <- c("Activity", "ActivityName")
+colnames(allSubjects) <- c("SubjectID")
+allData = cbind(allData, allLabels)
+allData = cbind(allData, allSubjects)
+
+# Create independent tidy data set with the average of each variable for each activity and each subject
+tidyData = ddply(allData, c("SubjectID", "ActivityName"), function (x) { 
+        colMeans(x[1:(length(allData)-3)])
+    })
+
+# Write tidyData to file
+write.table(tidyData, "TidyData.txt", row.names=FALSE)
+
+# Write codebook to file
+cbContent = paste("* ", names(tidyData), sep="")
+write.table(cbContent, file="CodeBook.md", quote=FALSE,
+            row.names=FALSE, col.names=FALSE, sep="\t")
